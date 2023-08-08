9bd11a456ebf85c71ba69423b7d2e302957de5ec
Aayush-Yadav29 <aayush.yadav.21031@iitgoa.ac.in>
2023-08-08 10:22:59 +0530

    adding about page

---
+++ src/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <h1 className="about-heading">ABOUT US</h1>
+      <div className="about-content">
+        Welcome to our Attendance Management Web App! We are dedicated to providing a seamless solution for tracking and managing attendance. Whether you're an educator striving for more efficient classroom management or an organization aiming to streamline attendance records, our user-friendly app offers real-time insights, accurate tracking, and customizable settings. Join us in simplifying attendance management and making informed decisions effortlessly.
+      </div>
+    </div>
+  );
+};
+
+export default About;
