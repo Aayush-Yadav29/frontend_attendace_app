5655274ccec3591f03bb7db4970548785a8ca792
Aayush-Yadav29 <aayush.yadav.21031@iitgoa.ac.in>
2023-08-10 23:23:50 +0530

    adding FacultySessionPage

--- src/App.js
+++ src/App.js
@@ -5,13 +5,22 @@ import ContactUs from './ContactUs';
 import Home from './Home';
 import Footer from './Footer';
 import SessionPage from './SessionPage';
+import FacultySessionPage from './FacultySessionPage';
 function App() {
+  const data= [
+    {Session : "Session 1", Time : "2:00pm" , Remark : "P"},
+    {Session : "Session 2", Time : "10:00am" , Remark : "A"},
+    {Session : "Session 3", Time : "3:00pm" , Remark : "P"},
+    {Session : "Session 4", Time : "8:00am" , Remark : "A"}
+  ]
+  
   return (
     <div className="App">
       {/* <Home/>
       <About/>
       <Footer/> */}
-      <SessionPage/>
+      {/* <SessionPage sessionlist={data}/> */}
+      <FacultySessionPage sessionlist={data}/>
       {/* <ContactUs/> */}  
     </div>
   );
---
+++ src/FacultySessionPage.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { Typography} from '@mui/material';
+
+export default function FacultySessionPage({ sessionlist }) {
+    const parentStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%',
+        border: '1px solid #ccc',
+        padding: '10px',
+        cursor:'pointer'
+      };
+    
+      const childStyle = {
+        flex: 1,
+        textAlign: 'center',
+      };
+    return (
+        <div className="FacultySessionPage" style={{ backgroundColor: "whitesmoke", paddingTop: '70px', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+
+            <div
+                className="banner"
+                style={{
+                    position: 'relative',
+                    width: '910px',
+                    maxWidth: '90%',
+                    height: '30%',
+                    border: '1px solid #ccc',
+                    backgroundColor: '#ffffff',
+                    marginBottom: '2%',
+                    boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px', // Adding box shadow for 3D effect
+                }}
+            >
+                <Typography variant="body1" style={{ position: 'absolute', bottom: 10, left: 10 }}>
+                    Artificial Intelligence
+                </Typography>
+                <Typography variant="body1" style={{ position: 'absolute', bottom: 30, left: 10 }}>
+                    CS330
+                </Typography>
+            </div>
+          
+            
+            <div className="content" style={{margin: 'auto', width: '920px', maxWidth: '90%' }}>
+            {sessionlist.map(item => (
+            <div style={parentStyle}>
+                <div style={childStyle}>{item.Session}</div>
+                <div style={childStyle}>{item.Time}</div>
+                <div style={{flex: 1,textAlign: 'center',color: item.Remark === 'P' ? 'green' : 'red' }}>{item.Remark}</div>
+                
+            </div>
+            ))}
+            </div>
+        </div>
+    );
+}
