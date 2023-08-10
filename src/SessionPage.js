c75df28f7a769d1d33820f305d3b64d004a99d3a
Aayush-Yadav29 <aayush.yadav.21031@iitgoa.ac.in>
2023-08-10 23:23:19 +0530

    adding SessionPage

--- src/SessionPage.js
+++ src/SessionPage.js
@@ -1,62 +1,51 @@
 import * as React from 'react';
-import { Typography, Avatar, ListItemAvatar, ListItemText, Divider, ListItem, List } from '@mui/material';
+import { Typography} from '@mui/material';
 
-export default function SessionPage() {
+export default function SessionPage({ sessionlist }) {
+    const parentStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: '100%',
+        border: '1px solid #ccc',
+        padding: '10px',
+      };
+    
+      const childStyle = {
+        flex: 1,
+        textAlign: 'center',
+        // border: '1px solid #ccc', 
+      };
     return (
-        <div className="SessionPage" style={{ backgroundColor: 'cyan', paddingTop: '70px',height:'100vh',display:'flex', flexDirection:'column',alignItems: 'center'}}>
+        <div className="SessionPage" style={{  paddingTop: '70px', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
 
-            <div className="banner" style={{ position: 'relative', width: '60%', height: '30%', border: '1px solid #ccc', backgroundColor: '#ffffff',marginBottom:'2%'}}>
-                <Typography
-                    variant="body1"
-                    style={{ position: 'absolute', bottom: 10, left: 10 }}
-                >
+            <div
+                className="banner"
+                style={{
+                    position: 'relative',   
+                    width: '910px',
+                    maxWidth:'90%',
+                    height: '30%',
+                    border: '1px solid #ccc',
+                    backgroundColor: '#ffffff',
+                    marginBottom: '2%',
+                    boxShadow: 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px', // Adding box shadow for 3D effect
+                }}
+            >
+                <Typography variant="body1" style={{ position: 'absolute', bottom: 10, left: 10 }}>
                     Artificial Intelligence
                 </Typography>
-                <Typography
-                    variant="body1"
-                    style={{ position: 'absolute', bottom: 30, left: 10 }}
-                >
+                <Typography variant="body1" style={{ position: 'absolute', bottom: 30, left: 10 }}>
                     CS330
                 </Typography>
             </div>
-            <List sx={{
-                width: '60%', bgcolor: 'background.paper'}}>
-                <ListItem alignItems="center">
-                    Session 1
-                    <Typography
-                        sx={{ display: 'inline' }}
-                        component="span"
-                        variant="body2"
-                        color="text.primary"
-                    >
-                        Time
-                    </Typography>
-                </ListItem>
-                <Divider variant="inset" component="li" />
-                <ListItem alignItems="center">
-                    <ListItemText
-                        primary="Summer BBQ"
-                        secondary={
-                            <React.Fragment>
-                                <Typography
-                                    sx={{ display: 'inline' }}
-                                    component="span"
-                                    variant="body2"
-                                    color="text.primary"
-                                >
-                                    to Scott, Alex, Jennifer
-                                </Typography>
-                                {" — Wish I could come, but I'm out of town this…"}
-                            </React.Fragment>
-                        }
-                    />
-                </ListItem>
-                
-                
-    
-        
-               
-            </List>
+
+            {sessionlist.map(item => (
+            <div style={parentStyle}>
+                <div style={childStyle}>{item.Session}</div>
+                <div style={childStyle}>{item.Time}</div>
+                <div style={childStyle}>{item.Remark}</div>
+            </div>
+            ))}
         </div>
     );
 }
